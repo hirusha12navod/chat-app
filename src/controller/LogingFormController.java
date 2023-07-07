2b2b64c3e0f1911f38f3afb1537e792ca469f121
hirusha12navod <hirushanavod789@gmail.com>
2023-07-07 12:55:09 +0530

    completed

---
+++ src/controller/LogingFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LogingFormController {
+
+    public JFXTextField Txt_Enter_Name;
+   static String User_name;
+
+    public void Txt_name_on_action(ActionEvent actionEvent) throws IOException {
+
+        User_name=Txt_Enter_Name.getText();
+        Txt_Enter_Name.clear();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(LogingFormController.class.getResource("../view/client.fxml"))));
+        stage.close();
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
